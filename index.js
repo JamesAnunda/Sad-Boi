cfe4fa838d6d5b75320a3e2098dd1362ee2d0410
Chris Anunda <james@jamesanunda.dev>
2022-12-21 01:56:32 -0500

    update

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config(); //initialize dotenv
 const Discord = require('discord.js'); //import discord.js
 
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, REST, Routes } = require('discord.js');
+const rest = new REST({ version: '14' }).setToken(CLIENT_TOKEN);
 //V14 use GatewayIntentBits
 const client = new Discord.Client({
   intents: [
@@ -11,19 +12,45 @@ const client = new Discord.Client({
   ]
 })
 
+//Commands
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+
 //When pinging bot directly
 client.on("messageCreate", (message) => {
   if (message.mentions.users.has(client.user.id)) { // this could be modified to make it have to *start* with or have only a ping
     const timeTaken = Date.now() - message.createdTimestamp;
-    message.reply("i dont know who the fuck or i am! This message had a latency of ${timeTaken}ms.")
+    console.log(timeTaken.toString());
+    message.reply(`i dont know who or what i am! This message had a latency of ${timeTaken} ms.`)
   } 
   console.log(message);
   });
-// client.on('messsageCreate', function {
-//   if (command === "ping") {
-//     const timeTaken = Date.now() - message.createdTimestamp;
-//     message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-//   }
+
+//Typing ping in the bot channel
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!'); 
+  }
+});
+
 
 //Connection to Discord API
 client.on('ready', () => {
@@ -31,5 +58,4 @@ client.on('ready', () => {
 });
 
 //make sure this line is the last line
-client.login(process.env.CLIENT_TOKEN); //login bot using token
-
+client.login(process.env.CLIENT_TOKEN); //login bot using tokenizer 
